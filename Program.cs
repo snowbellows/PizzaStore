5fb0ae6cb08b2c043499834fed1b933a9ec3b00d
Alexis Halverson <alexhalverson@posteo.net>
2025-09-21 16:53:59 +1000

    feat: first commit minimal api

---
+++ Program.cs
@@ -0,0 +1,37 @@
+using Microsoft.OpenApi.Models;
+using PizzaStore.DB;
+
+
+var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddCors(options => { });
+
+builder.Services.AddEndpointsApiExplorer();
+
+if (builder.Environment.IsDevelopment())
+{
+    builder.Services.AddSwaggerGen(c =>
+    {
+        c.SwaggerDoc("v1", new OpenApiInfo { Title = "PizzaStore API", Description = "Keep track of your pizzas", Version = "v1" });
+    });
+
+}
+
+var app = builder.Build();
+
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI(c =>
+    {
+        c.SwaggerEndpoint("/swagger/v1/swagger.json", "PizzaStore API V1");
+    });
+}
+
+app.MapGet("/pizzas/{id}", (int id) => PizzaDB.GetPizza(id));
+app.MapGet("/pizzas", () => PizzaDB.GetPizzas());
+app.MapPost("/pizzas", (Pizza pizza) => PizzaDB.CreatePizza(pizza));
+app.MapPut("/pizzas", (Pizza pizza) => PizzaDB.UpdatePizza(pizza));
+app.MapDelete("/pizzas/{id}", (int id) => PizzaDB.RemovePizza(id));
+
+app.Run();

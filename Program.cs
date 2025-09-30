002eb825809bc6e318e88ee4b868b715c67fe33b
Alex Halverson <alexhalverson@posteo.net>
2025-09-30 12:49:51 +1000

    feat(db): swap to using SQLite instead of in memory db

--- Program.cs
+++ Program.cs
@@ -4,9 +4,12 @@ using PizzaStore.Models;
 
 var builder = WebApplication.CreateBuilder(args);
 
+var connectionString =
+    builder.Configuration.GetConnectionString("Pizzas") ?? "Data Source=Pizzas.db";
+
 builder.Services.AddEndpointsApiExplorer();
 
-builder.Services.AddDbContext<PizzaDB>(options => options.UseInMemoryDatabase("items"));
+builder.Services.AddSqlite<PizzaDB>(connectionString);
 
 if (builder.Environment.IsDevelopment())
 {
